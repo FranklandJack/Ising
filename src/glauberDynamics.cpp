742e3b092296354c182196c8314242e93cf209e4
Jack Frankland <jackfrankland@jacks-mbp.home>
2018-01-21 11:02:32 +0000

    fixed indexing problem

--- src/glauberDynamics.cpp
+++ src/glauberDynamics.cpp
@@ -1,8 +1,8 @@
 #include "glauberDynamics.hpp"
 void glauberDynamics(SpinLattice2D& spinLattice, std::default_random_engine generator, double jConstant)
 {
-	std::uniform_int_distribution<int> rowDistriubution(0,spinLattice.getRows());
-	std::uniform_int_distribution<int> colDistriubution(0,spinLattice.getCols());
+	std::uniform_int_distribution<int> rowDistriubution(0,spinLattice.getRows()-1);
+	std::uniform_int_distribution<int> colDistriubution(0,spinLattice.getCols()-1);
 
 	int row = rowDistriubution(generator);
 	int col = colDistriubution(generator);
