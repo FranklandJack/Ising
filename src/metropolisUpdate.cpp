d1a8b7b2776e214addf49b56deede7d415ef5395
Jack Frankland <jackfrankland@jacks-mbp.home>
2018-01-21 10:43:26 +0000

    implemented metroplois update function

---
+++ src/metropolisUpdate.cpp
@@ -0,0 +1,7 @@
+#include "metropolisUpdate.hpp"
+bool metropolisUpdate(double energyBefore, double energyAfter, std::default_random_engine generator)
+{
+	double deltaEnergy = energyAfter - energyBefore;
+	std::uniform_real_distribution<double> distribution(0.0,1.0);
+	return distribution(generator) <= std::min(1.0,-deltaEnergy);
+}
--- src/metropolisUpdate.hpp
+++ src/metropolisUpdate.hpp
@@ -1,4 +1,6 @@
 #ifndef metropolisUpdate_hpp
 #define metropolisUpdate_hpp
-
+#include <random>
+#include <cmath>
+bool metropolisUpdate(double, double, std::default_random_engine);
 #endif /* metropolisUpdate_hpp */
